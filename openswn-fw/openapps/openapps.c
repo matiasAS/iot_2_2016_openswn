9871233e3c732ed352173b656c322a614c0710ac
matiasAS <matialvarezs@gmail.com>
2016-11-22 00:50:17 -0300

    todo

---
+++ openswn-fw/openapps/openapps.c
@@ -0,0 +1,47 @@
+/**
+\brief Applications running on top of the OpenWSN stack.
+
+\author Thomas Watteyne <watteyne@eecs.berkeley.edu>, September 2014.
+*/
+
+#include "opendefs.h"
+
+// CoAP
+#include "c6t.h"
+#include "cinfo.h"
+#include "cleds.h"
+#include "cexample.h"
+#include "cstorm.h"
+#include "cwellknown.h"
+#include "rrt.h"
+//#include "csensors.h" //****
+// TCP
+#include "techo.h"
+// UDP
+#include "uecho.h"
+#include "uinject.h"
+
+
+//=========================== variables =======================================
+
+//=========================== prototypes ======================================
+
+//=========================== public ==========================================
+
+//=========================== private =========================================
+
+void openapps_init(void) {
+   // CoAP
+   c6t_init();
+   cinfo_init();
+   csensors_init();
+   cexample_init();
+   cleds__init();
+   cstorm_init();
+   cwellknown_init();
+   rrt_init();
+   // TCP
+   techo_init();
+   // UDP
+//   uecho_init();
+}
